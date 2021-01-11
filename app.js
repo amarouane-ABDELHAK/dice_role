b2a78171818f1c8a8bc80631b764ffb7bd6d5404
amarouane-ABDELHAK <am0089@uah.edu>
2021-01-10 20:59:25 -0600

    dice it

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 function rollDice() {
+  document.getElementById("dice12").style.visibility = "hidden";
+  document.getElementById("dice6").style.visibility = "visible";
+  num = getRandomNumber(1, 12);
+  if (num > 6) {
+    document.getElementById("dice12").style.visibility = "visible";
+    document.getElementById("dice6").style.visibility = "hidden";
+  }
 
-  document.getElementById("die-1").style.display = "grid";
-  document.getElementById("die-3").style.display = "none";
+
 
   const dice = [...document.querySelectorAll(".die-list")];
+
+  
 
   dice.forEach(die => {
     console.log(die.id);
   
     
-    
-    num = getRandomNumber(1, 12);
-    if (num > 6 && die.id == "die-1") {
-      document.getElementById("die-1").style.display = "none";
-    document.getElementById("die-3").style.display = "grid";
-    }
     toggleClasses(die);
     die.dataset.roll = getRandomNumber(1, 6);
-
-      
-  
-    
 
     
   });
